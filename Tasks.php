4c288e7c1dbfdb71521d1051d242d9ba8e8e814c
mohamedatef542001 <mohamedatef54200@gmail.com>
2022-07-29 08:35:18 +0200

    Done

--- Tasks.php
+++ Tasks.php
@@ -1,28 +1,45 @@
 <?php
 
-    session_start();
-    $var_log = $_SESSION['logged'];
-    $em = explode('@',$_SESSION['email']);
-    $email = $em[0];
-    if (!$email) {
-        $er[] = "You must login To Access Dashboard";
-        $_SESSION['err'] = $er; 
-        header("location: index.php");
+session_start();
+$var_log = $_SESSION['logged'];
+$em = explode('@', $_SESSION['email']);
+$email = $em[0];
+if (!$email) {
+    $er[] = "You must login To Access Dashboard";
+    $_SESSION['err'] = $er;
+    header("location: index.php");
+}
+
+$directory = "./uploads";
+
+if (!is_dir($directory)) {
+    exit('Invalid diretory path');
+}
+
+$files = array();
+foreach (scandir($directory) as $file) {
+    if ($file !== '.' && $file !== '..') {
+        $file = explode('.',$file);
+        $file = $file[0];
+        $files[] = $file;
     }
- 
-
-
+}
 
 ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./styles/main.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Tasks</title>
+    <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css'>
+    <link rel="stylesheet" href="./styles/main.css">
+    <link rel="stylesheet" href="./styles/task.css">
 </head>
+
 <body>
     <header>
         <nav id="nav-bar">
@@ -35,8 +52,45 @@
             </ul>
         </nav>
     </header>
-     <div>
-     <h1 class="h1-name"><?= "Welcome, ",$email ?></h1>
-     </div>
+    <div>
+        <h1 class="h1-name"><?= "Welcome, ", $email ?></h1>
+    </div>
+
+    <div class="wrapper center-block">
+        <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+
+            <?php
+
+            foreach ($files  as $key => $value) {
+                echo "
+                        <div class='panel panel-default'>
+                        <div class='panel-heading active' role='tab' id='headingOne'>
+                            <h4 class='panel-title'>
+                                <a role='button' data-toggle='collapse' data-parent='#accordion' href='#$key' aria-expanded='true' aria-controls='collapseOne'>
+                                    $value
+                                </a>
+                            </h4>
+                        </div>
+                        <div id='$key' class='panel-collapse collapse in' role='tabpanel' aria-labelledby='heading1'>
+                            <div class='panel-body'>
+                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                            </div>
+                        </div>
+                    </div>
+                        
+                        ";
+            }
+
+            ?>
+
+
+        </div>
+    </div>
+
+
+    <script src='https://code.jquery.com/jquery-2.2.4.min.js'></script>
+    <script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js'></script>
+    <script src="./js/script.js"></script>
 </body>
+
 </html>
--- dashboard.php
+++ dashboard.php
@@ -1,29 +1,36 @@
 <?php
 
-    session_start();
-    $var_log = $_SESSION['logged'];
-    $em = explode('@',$_SESSION['email']);
-    $email = $em[0];
-    if ($email) {
-        if (isset($_POST['submit'])) {
-           if (!empty($_FILES['upload']['name'])) {
-                $file_name = $_FILES['upload']['name'];
-                $file_tmp = $_FILES['upload']['tmp_name'];
-                $target_dir = "uploads/${file_name}";
-                move_uploaded_file($file_tmp,$target_dir);
-                $msg = "<h4 style='color:green'>File Uploaded Successfully !</h4>";
+session_start();
+$allowed_files = ['php','txt','js','css'];
+$var_log = $_SESSION['logged'];
+$em = explode('@', $_SESSION['email']);
+$email = $em[0];
+if ($email) {
+    if (isset($_POST['submit'])) {
+        if (!empty($_FILES['upload']['name'])) {
+            $file_name = $_FILES['upload']['name'];
+            $file_tmp = $_FILES['upload']['tmp_name'];
+            $target_dir = "Tasks/${file_name}";
+            $file_ext = explode('.',$file_name);
 
-           }
-           else{
-                $msg = "<h4 style='color:red'>You must choose file to upload</h4>";
-           }
-        }   
+            if (in_array($file_ext[1],$allowed_files)) {
+                move_uploaded_file($file_tmp, $target_dir);
+                $msg = "<h4 style='color:green'>File Uploaded Successfully !</h4>";
+                
+            }
+            else{
+                $msg = "<h4 style='color:red'>You must choose with .ext from [php','txt','js','css'] !</h4>";
+            }
+        } 
+        else {
+            $msg = "<h4 style='color:red'>You must choose file to upload</h4>";
+        }
     }
-    else{
-        $er[] = "You must login To Access Dashboard";
-        $_SESSION['err'] = $er; 
-        header("location: index.php");
-    }
+} else {
+    $er[] = "You must login To Access Dashboard";
+    $_SESSION['err'] = $er;
+    header("location: index.php");
+}
 
 
 
@@ -31,6 +38,7 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -38,6 +46,7 @@
     <link rel="stylesheet" href="./styles/main.css">
     <title>Tasks</title>
 </head>
+
 <body>
     <header>
         <nav id="nav-bar">
@@ -50,19 +59,22 @@
             </ul>
         </nav>
     </header>
-     <div>
-     <h1 class="h1-name"><?= "Welcome, ",$email ?></h1>
-     </div>
+    <div>
+        <h1 class="h1-name"><?= "Welcome, ", $email ?></h1>
+    </div>
     <section>
-        
-        <form action="<?= $_SERVER['PHP_SELF']?>" method="post" enctype="multipart/form-data">
+
+        <div class="contain">
+        <form action="<?= $_SERVER['PHP_SELF'] ?>" method="post" enctype="multipart/form-data">
             <?= $msg ?? null ?>
-        Choose File To Upload : 
-        <input type="file" name="upload" id="upload">
-        <input type="submit" name="submit" class="btn">
-       </div>
-    </form>
+            Choose File To Upload :
+            <input type="file" name="upload" id="upload">
+            <input type="submit" name="submit" class="btn">
+            </div>
+        </form>
+        </div>
     </section>
 
 </body>
+
 </html>
